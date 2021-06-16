be1f7702d11df0032779ac48b84a7a18bb1ff12a
tora_pan <travis.pandos@gmail.com>
2021-06-15 21:08:52 -0700

    set up loading data

--- BlazorServer/Startup.cs
+++ BlazorServer/Startup.cs
@@ -1,4 +1,5 @@
 using BlazorServer.Data;
+using DataLibrary;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -29,6 +30,7 @@ namespace BlazorServer
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<IDataAccess, DataAccess>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
